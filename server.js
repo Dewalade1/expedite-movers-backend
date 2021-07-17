9a8ba155a2572a176ab927b21be2af4d2ddb6a5c
Dewa Ade-onojobi <adewalade@gmail.com>
2021-07-17 16:16:54 +0100

    chore: setup moesif for api call tracking

--- server.js
+++ server.js
@@ -3,12 +3,31 @@ require("dotenv").config({path: './.env'})
 const cors = require("cors")
 const express = require("express")
 const bodyParser = require("body-parser")
+const moesifExpress = require('moesif-express');
 
 const mainRoute = require("./routes/main.route")
 
 const app = express()
 
 const port = process.env.PORT || 8080;
+
+const options = {
+
+  applicationId: process.env.MOESIF_APPLICATION_ID,
+
+  identifyUser: function (req, res) {
+    if (req.user) {
+      return req.user.id;
+    }
+    return undefined;
+  },
+
+  getSessionToken: function (req, res) {
+    return req.headers['Authorization'];
+  }
+};
+
+app.use(moesifExpress(options));
 
 app.use(bodyParser.json())
 
