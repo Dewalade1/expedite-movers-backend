dc9de6462c24d1c71ffabb9cc56c0183dbcb0661
Dewa Ade-onojobi <adewalade@gmail.com>
2021-07-21 22:58:24 -0200

    feat: finish input validation logic

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,14 +2,17 @@ const { body, validationResult } = require("express-validator");
 
 const messagesService = require("../services/messages.service");
 
+// const { recieverNotifData , senderNotifData } = require("../data/dynamicTemplate.data");
+
 exports.validate = ( method ) => {
     switch ( method ) {
         case "sendMessageToEmail": {
             return [
-                body("name", 'No name was entered').exists().toLowerCase().isString(),
-                body("email", "You should enter a valid email").exists().isEmail().normalizeEmail(),
-                body("message", "There is no message to send").exists().isString().not().notEmpty().isLength({
-                    max: 250
+                body("name", 'No name was entered').exists().notEmpty().toLowerCase().isString(),
+                body("email", "Please enter a valid email").exists().isEmail().normalizeEmail(),
+                body("message", "There must be a message with length no more than 300 characters")
+                .exists().isString().notEmpty().isLength({
+                    max: 300
                 }).trim().escape(),
             ]
         }    
@@ -35,58 +38,58 @@ exports.sendMessageToEmail = async (req, res, next) => {
             });
         }
 
-    const emails = [
-        // send notification emails to official expeditemovers email accounts
-        {
-            to: ['Olubowale Ade-Onojobi <adewalade@gmail.com>', 'Team team@ellopod.com'],
-            from: 'noreply@expeditemoversng.com',
-            templateId: 'd-fbc401c7877e467a810ceb678a220caf',
-            dynamicTemplateData: {
+        const emails = [
+            // send notification emails to official expeditemovers email accounts
+            {
+                to: ['Olubowale Ade-Onojobi <adewalade@gmail.com>', 'Team <team@ellopod.com>'],
+                from: 'noreply@expeditemoversng.com',
+                templateId: 'd-fbc401c7877e467a810ceb678a220caf',
+                dynamicTemplateData: {
+                        "receiver": {
+                            "firstName": "Dewa",
+                            "lastName": "Support",
+                            "email": "adewalade@gmail.com" 
+                    },
+                        "contact": {
+                            "name": req.body.name,
+                            "email": req.body.email,
+                            "message": req.body.message
+                    },
+                },
+            },
+            // send emails to senders of website messages
+            {
+                to: req.body.email,  // send different emails to multiple people
+                from: 'noreply@expeditemoversng.com',
+                templateId: 'd-82ef6793262e4453a1690d367d8eba95',
+                dynamicTemplateData: {
                     "receiver": {
-                    "firstName": "Team",
-                    "lastName": "Support",
-                    "email": "contact@expeditemoversng.com" 
-                },
-                    "contact": {
-                    "name": req.body.name,
-                    "email": req.body.email,
-                    "message": req.body.message
+                        "firstName": req.body.name,
+                        "lastName": req.body.lastName,
+                        "email": req.body.email 
+                    },
                 },
             },
-        },
-        // send emails to senders of website messages
-        {
-            to: 'Receiver Brown <adewalade@gmail.com>',  // send different emails to multiple people
-            from: 'noreply@expeditemoversng.com',
-            templateId: 'd-82ef6793262e4453a1690d367d8eba95',
-            dynamicTemplateData: {
-                "receiver": {
-                    "firstName": req.body.name,
-                    "lastName": req.body.lastName,
-                    "email": req.body.email 
-                },
-            },
-        },
-    ]
+        ]
 
-    messagesService.sendMessageToEmail(emails, (error, results) => {
+        messagesService.sendMessageToEmail(emails, (error, results) => {
 
-        if ( error )   {
-            return res.status(400).send({
-                success: false,
-                data: null,
-                errors: error
-            })
-        }
+            if ( error )   {
+                return res.status(400).send({
+                    success: false,
+                    data: null,
+                    errors: error
+                })
+            }
 
-        return(
-            res.status(200).send({
-                success: true,
-                data: results,
-                errors: error
-            })
-        )
-    })
+            return(
+                res.status(200).send({
+                    success: true,
+                    data: results,
+                    errors: error
+                })
+            )
+        })
 
     } catch (err) {
         return next(err);
